393661a6641fd43c294d54efd7cee8ba5a158c04
ugesh <ugesh.roopchand@hotmail.com>
2019-06-28 15:07:34 +0400

    Simple Save Category Method

--- Modist.Services/CategoriesService.cs
+++ Modist.Services/CategoriesService.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using Modist.Database;
+using Modist.Entities;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -9,8 +11,24 @@ namespace Modist.Services
     public class CategoriesService
     {
 
+        public List<Category> GetAllCategories()
+        {
+            using (var context = new ApplicationDbContext())
+            {
+                return context.categories.ToList();
+            }
+        }
 
 
+       public void SaveCategory(Category category)
+        {
+            using (var context = new ApplicationDbContext())
+            {
+                context.categories.Add(category);
+                context.SaveChanges();
+
+            }
+        }
 
     }
 }
---
+++ Modist/Controllers/CategoryController.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using Modist.Services;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Modist.Entities;
+
+namespace Modist.Controllers
+{
+    public class CategoryController : Controller
+    {
+
+        CategoriesService sc = new CategoriesService();
+
+
+        // GET: Category
+        public ActionResult Index()
+        {
+            var ListOfCategory = sc.GetAllCategories();
+            return View(ListOfCategory);
+        }
+
+        [HttpGet]
+        public ActionResult Create ()
+        {
+            
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Create(Category category)
+        {
+            sc.SaveCategory(category);
+
+            return View();
+        }
+    }
+}
